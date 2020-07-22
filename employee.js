2044b5313a5b6a5d8936baca6393bc08fc88d547
Laura Lara <llaraama@uncc.edu>
2020-07-21 20:45:18 -0400

    added a select role and manager function to be able to put it in the add employee section of the code later in the project

--- employee.js
+++ employee.js
@@ -78,3 +78,26 @@ function start() {
       });
   }
 
+  var roleArr = [];
+  function selectRole() {
+    connection.query("SELECT * FROM roles", function(err, res) {
+      if (err) throw err
+      for (var i = 0; i < res.length; i++) {
+        roleArr.push(res[i].title);
+      }
+    })
+    return roleArr;
+  }
+  
+  var managersArr = [];
+  function selectManager() {
+    connection.query("SELECT first_name, last_name FROM employees WHERE manager_id IS NULL", function(err, res) {
+      if (err) throw err
+      for (var i = 0; i < res.length; i++) {
+        managersArr.push(res[i].first_name);
+      }
+    })
+    return managersArr;
+  }
+  
+  
